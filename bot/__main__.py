8ecae09034732aa74c573fc4dfd4e96750ddb097
saucedbeacon <67328305+saucedbeacon@users.noreply.github.com>
2021-10-09 16:14:17 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -12,7 +12,7 @@ def work(client, message):
     caption = None
     msg = None
     if remove_strings:
-      if message.caption in remove_strings:
+      if remove_strings in message.caption:
         if message.media and not message.poll:
           print("BlacklistHitted")
           print(message)
