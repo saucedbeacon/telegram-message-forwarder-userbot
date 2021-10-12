9406f49197721a147061c803ad400b0bea33a98e
saucedbeacon <67328305+saucedbeacon@users.noreply.github.com>
2021-10-12 23:48:53 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -33,7 +33,6 @@ def validator(client, message) :
                 #LawanCOVID19
                 
                 ref: MENFESS_SUCCESS_SENT""")
-                app.send_message(-1001450732898, str(message))
                 if advance_config:
                   try:
                     for chat in chats_data[message.chat.id]:
