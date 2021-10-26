c0408f9d9ba5e61082197aa7ae8f7c5c69e67cd5
saucedbeacon <67328305+saucedbeacon@users.noreply.github.com>
2021-10-26 14:58:21 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -162,7 +162,7 @@ def antiLink(Client, message):
     elif len(message.entities) == 1:
       delay(Client,message)
 
-@app.on_message(filters.chat(2056449872) & filters.incoming & filters.regex("#df") & filters.regex("#tanya") & filters.regex("#curhat") & filters.regex("#pamer"))
+@app.on_message(filters.chat(2056449872) & filters.incoming & filters.regex("#df") | filters.regex("#tanya") | filters.regex("#curhat") | filters.regex("#pamer"))
 
 def frPhoto(Client, message):
   message.copy(-1001183067327)
